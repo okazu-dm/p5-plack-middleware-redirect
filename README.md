e1ea6ce05863ea8e89ce15d4cd294c472d9c3379
okazu-dm <uhavetwocows@gmail.com>
2015-01-18 21:25:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Middleware::Redirect - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::Redirect;
+
+# DESCRIPTION
+
+Plack::Middleware::Redirect is ...
+
+# LICENSE
+
+Copyright (C) okazu-dm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+okazu-dm <uhavetwocows@gmail.com>
---
+++ lib/Plack/Middleware/Redirect.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Redirect;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Redirect - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::Redirect;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Redirect is ...
+
+=head1 LICENSE
+
+Copyright (C) okazu-dm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+okazu-dm E<lt>uhavetwocows@gmail.comE<gt>
+
+=cut
+
